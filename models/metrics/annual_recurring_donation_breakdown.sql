90985c68d6d176c88cb634da2f2320ace32183e8
ag-arg <78564909+ag-arg@users.noreply.github.com>
2022-03-17 15:29:26 -0500

    Create annual_recurring_donation_breakdown.sql

---
+++ models/metrics/annual_recurring_donation_breakdown.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='table') }}
+
+with
+
+donations as (
+  select * from {{ref ('recurring_donations') }}
+
+),
+
+all_donations as (
+    select
+      donation_id,
+      committee_name,
+      committee_id,
+      amount,
+      date_part_year(paid_at::date) as year
+    from donations
+
+  ),
+
+annual_recurring_fundraising_breakdown as (
+      select
+       year,
+       committee_id,
+       committee_name,
+       count(donation_id) as num_contributions,
+       sum(amount) as total_amount,
+       total_amount / num_contributions as avg_amount
+      from all_donations
+      group by 1,2,3
+    )
+
+  select * from annual_recurring_fundraising_breakdown
