81313c487d74b81beeefad0e34a715239a420adc
ag-arg <78564909+ag-arg@users.noreply.github.com>
2022-03-21 15:29:14 -0500

    Create conversion_rate.sql

---
+++ dashboard/winning_the_ccc/conversion_rate.sql
@@ -0,0 +1,5 @@
+select avg(attendance_conversion_rate) as avg_attendance_conversion_rate
+from sunrise_dbt.events_summary
+where 
+  event_name like '%Winning the Civilian Climate Corps'
+  or event_name like '%Winning the CCC'
